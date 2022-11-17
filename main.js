a1228078e8050616ee112663a0a689eaf134af4e
arnor9 <arg82@hi.is>
2022-11-17 12:53:47 +0000

    myndir

--- main.js
+++ main.js
@@ -4,28 +4,7 @@ import { setupCounter } from './counter.js'
 
 
 
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-  <section>
-  <div class = "merki"><a href="sida.html"><img src="liverpool.png" class="logo" alt="Liverpool logo"/></a></div>
-    <h1>Hversu margir halda með Liverpool</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-       ýttu á takkann ef þú heldur með Liverpool!
-    </p>
-    </section>
-    <h2>Saga Liverpool</h2>
-    <p>Liverpool Football Club is a professional football club</p> 
-    <p>based in Liverpool, England. The club competes in the Premier League,</p>
-    <p>the top tier of English football. Founded in 1892, the club joined the Football League the following year and has played its home games at Anfield since its formation.</p>
-    <img src="anfield.jpg" alt=" mynd af Anfield" />
-  </div>
   
-`
 
 setupCounter(document.querySelector('#counter'))
 
